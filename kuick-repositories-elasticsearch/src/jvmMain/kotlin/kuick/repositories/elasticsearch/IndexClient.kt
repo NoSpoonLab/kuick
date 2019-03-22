9c93546d27855fc88280471c1af953940844e4d9
soywiz <soywiz@gmail.com>
2019-03-22 12:12:30 +0100

    Small fixes

--- kuick-repositories-elasticsearch/src/jvmMain/kotlin/kuick/repositories/elasticsearch/IndexClient.kt
+++ kuick-repositories-elasticsearch/src/jvmMain/kotlin/kuick/repositories/elasticsearch/IndexClient.kt
@@ -1,30 +1,24 @@
 package kuick.repositories.elasticsearch
 
 import com.google.inject.*
-import kuick.json.Json
-import kuick.utils.getOrNull
-import org.elasticsearch.action.ActionListener
-import org.elasticsearch.action.ActionResponse
-import org.elasticsearch.action.admin.indices.create.CreateIndexRequest
-import org.elasticsearch.action.admin.indices.get.GetIndexRequest
-import org.elasticsearch.action.bulk.BulkRequest
-import org.elasticsearch.action.delete.DeleteRequest
-import org.elasticsearch.action.get.GetRequest
-import org.elasticsearch.action.get.GetResponse
-import org.elasticsearch.action.index.IndexRequest
-import org.elasticsearch.action.search.SearchRequest
-import org.elasticsearch.action.search.SearchResponse
+import kuick.di.*
+import kuick.json.*
+import org.elasticsearch.action.*
+import org.elasticsearch.action.admin.indices.create.*
+import org.elasticsearch.action.admin.indices.get.*
+import org.elasticsearch.action.bulk.*
+import org.elasticsearch.action.delete.*
+import org.elasticsearch.action.get.*
+import org.elasticsearch.action.index.*
+import org.elasticsearch.action.search.*
 import org.elasticsearch.action.support.*
-import org.elasticsearch.client.RequestOptions
-import org.elasticsearch.client.RestHighLevelClient
-import org.elasticsearch.common.xcontent.XContentType
-import org.elasticsearch.index.query.QueryBuilder
-import org.elasticsearch.search.builder.SearchSourceBuilder
+import org.elasticsearch.client.*
+import org.elasticsearch.common.xcontent.*
+import org.elasticsearch.index.query.*
+import org.elasticsearch.search.builder.*
 import javax.inject.Inject
 import javax.inject.Singleton
-import kotlin.coroutines.resume
-import kotlin.coroutines.resumeWithException
-import kotlin.coroutines.suspendCoroutine
+import kotlin.coroutines.*
 
 data class ElasticSearchConfig(
         val waitRefresh: Boolean = false
@@ -32,8 +26,8 @@ data class ElasticSearchConfig(
 
 @Singleton
 open class IndexClient @Inject constructor(
-    private val client: RestHighLevelClient,
-    private val injector: Injector
+        private val client: RestHighLevelClient,
+        private val injector: Injector
 ) {
 
     val config = injector.getOrNull<ElasticSearchConfig>()
@@ -47,37 +41,37 @@ open class IndexClient @Inject constructor(
         val request = GetIndexRequest()
         request.indices(indexName)
         return client.indices()
-            .exists(request, RequestOptions.DEFAULT)
+                .exists(request, RequestOptions.DEFAULT)
     }
 
     suspend fun createIndex(indexName: String, mapping: Map<String, Any> = emptyMap()) = suspendCoroutine<Unit> { c ->
         client.indices()
-            .createAsync(
-                CreateIndexRequest(indexName).mapping(
-                    DOCUMENT_TYPE, mapOf(DOCUMENT_TYPE to mapping)
-                ),
-                RequestOptions.DEFAULT,
-                actionListener(
-                    { c.resume(Unit) },
-                    { c.resumeWithException(it) }
+                .createAsync(
+                        CreateIndexRequest(indexName).mapping(
+                                DOCUMENT_TYPE, mapOf(DOCUMENT_TYPE to mapping)
+                        ),
+                        RequestOptions.DEFAULT,
+                        actionListener(
+                                { c.resume(Unit) },
+                                { c.resumeWithException(it) }
+                        )
                 )
-            )
     }
 
     suspend fun get(indexName: String, id: String): GetResponse = suspendCoroutine { c ->
         val getRequest = GetRequest(indexName, DOCUMENT_TYPE, id)
         client.getAsync(getRequest, RequestOptions.DEFAULT, actionListener(
-            { c.resume(it) },
-            { c.resumeWithException(it) }
+                { c.resume(it) },
+                { c.resumeWithException(it) }
         ))
 
     }
 
     suspend fun search(
-        indexName: String,
-        query: QueryBuilder,
-        from: Int? = null,
-        size: Int? = null
+            indexName: String,
+            query: QueryBuilder,
+            from: Int? = null,
+            size: Int? = null
     ): SearchResponse = suspendCoroutine { c ->
         val searchSourceBuilder = SearchSourceBuilder()
         searchSourceBuilder.query(query)
@@ -89,20 +83,20 @@ open class IndexClient @Inject constructor(
         searchRequest.types(DOCUMENT_TYPE)
 
         client.searchAsync(searchRequest, RequestOptions.DEFAULT, actionListener(
-            { c.resume(it) },
-            { c.resumeWithException(it) }
+                { c.resume(it) },
+                { c.resumeWithException(it) }
         ))
     }
 
 
     suspend fun <T : Any> add(indexName: String, id: String, item: T): T = suspendCoroutine { c ->
         val request = IndexRequest(indexName, DOCUMENT_TYPE, id)
-            .source(Json.toJson(item), XContentType.JSON).apply {
-                if (waitRefresh) refreshPolicy = WriteRequest.RefreshPolicy.IMMEDIATE
-            }
+                .source(Json.toJson(item), XContentType.JSON).apply {
+                    if (waitRefresh) refreshPolicy = WriteRequest.RefreshPolicy.IMMEDIATE
+                }
         client.indexAsync(request, RequestOptions.DEFAULT, actionListener(
-            { c.resume(item) },
-            { c.resumeWithException(it) }
+                { c.resume(item) },
+                { c.resumeWithException(it) }
         ))
     }
 
@@ -110,13 +104,13 @@ open class IndexClient @Inject constructor(
         val bulkRequest = createBulkRequest()
         items.forEach {
             bulkRequest.add(
-                IndexRequest(indexName, DOCUMENT_TYPE, idFunction(it))
-                    .source(Json.toJson(it), XContentType.JSON)
+                    IndexRequest(indexName, DOCUMENT_TYPE, idFunction(it))
+                            .source(Json.toJson(it), XContentType.JSON)
             )
         }
         client.bulkAsync(bulkRequest, RequestOptions.DEFAULT, actionListener(
-            { c.resume(Unit) },
-            { c.resumeWithException(it) }
+                { c.resume(Unit) },
+                { c.resumeWithException(it) }
         ))
     }
 
@@ -125,8 +119,8 @@ open class IndexClient @Inject constructor(
             if (waitRefresh) refreshPolicy = WriteRequest.RefreshPolicy.IMMEDIATE
         }
         client.deleteAsync(deleteRequest, RequestOptions.DEFAULT, actionListener(
-            { c.resume(Unit) },
-            { c.resumeWithException(it) }
+                { c.resume(Unit) },
+                { c.resumeWithException(it) }
         ))
     }
 
@@ -139,8 +133,8 @@ open class IndexClient @Inject constructor(
                     bulkRequest.add(DeleteRequest(indexName, DOCUMENT_TYPE, it.id))
                 }
                 client.bulkAsync(bulkRequest, RequestOptions.DEFAULT, actionListener(
-                    { response -> c.resume(response.items.map { it.id }) },
-                    { c.resumeWithException(it) }
+                        { response -> c.resume(response.items.map { it.id }) },
+                        { c.resumeWithException(it) }
                 ))
             }
         } else {
@@ -153,9 +147,9 @@ open class IndexClient @Inject constructor(
     }
 
     private fun <T : ActionResponse> actionListener(onResponse: (T) -> Unit, onFailure: (Exception) -> Unit): ActionListener<T> =
-        object : ActionListener<T> {
-            override fun onResponse(response: T?) = onResponse(response!!)
-            override fun onFailure(e: Exception?) = onFailure(e!!)
-        }
+            object : ActionListener<T> {
+                override fun onResponse(response: T?) = onResponse(response!!)
+                override fun onFailure(e: Exception?) = onFailure(e!!)
+            }
 
 }
