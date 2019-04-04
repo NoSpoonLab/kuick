2dd29742e66f19003e926a0462608b19e8b622bd
soywiz <soywiz@gmail.com>
2019-04-04 16:35:00 +0200

    Redis client with kotlin suspend wrapper + RedisCache + Cache.get interface fix + tests

---
+++ kuick-cache-redis/src/jvmMain/kotlin/kuick/caching/redis/RedisCache.kt
@@ -0,0 +1,32 @@
+package kuick.caching.redis
+
+import io.lettuce.core.*
+import kuick.client.redis.*
+import kuick.json.*
+import kotlin.reflect.*
+
+class RedisCache<V : Any> @PublishedApi internal constructor(
+        private val redis: RedisSuspendCommands<String, String>,
+        private val cacheName: String,
+        private val clazz: KClass<V>
+) : Cache<String, V> {
+    companion object {
+        suspend inline operator fun <reified V : Any> invoke(cacheName: String, uri: RedisURI = RedisURI.create("redis://localhost/"), client: RedisClient = RedisClient.create()): RedisCache<V> {
+            return RedisCache(client.connectSuspend(uri).suspending(), cacheName, V::class)
+        }
+    }
+
+    override suspend fun get(key: String, builder: suspend () -> V): V {
+        val value = redis.hget(cacheName, key) ?: return builder().also { redis.hset(cacheName, key, Json.toJson(it)) }
+        return Json.fromJson(value, clazz)
+    }
+
+    override suspend fun invalidate(key: String) {
+        redis.hdel(cacheName, key)
+    }
+}
+
+interface Cache<K : Any, T : Any> {
+    suspend fun get(key: K, builder: suspend () -> T): T
+    suspend fun invalidate(key: K)
+}
---
+++ kuick-cache-redis/src/jvmTest/kotlin/kuick/caching/redis/RedisCacheTest.kt
@@ -0,0 +1,33 @@
+package kuick.caching.redis
+
+import io.lettuce.core.*
+import kotlinx.coroutines.*
+import org.arquillian.cube.containerobject.*
+import org.arquillian.cube.docker.impl.client.containerobject.dsl.*
+import org.jboss.arquillian.junit.*
+import org.junit.runner.*
+import kotlin.test.*
+
+@RunWith(Arquillian::class)
+class RedisCacheTest {
+    @DockerContainer
+    var container = Container.withContainerName("kuick-redis-test")
+            .fromImage("redis:5.0.4")
+            .withPortBinding(6379)
+            .withEnvironment("discovery.type", "single-node")
+            .withConnectionMode(ConnectionMode.START_AND_STOP_AROUND_CLASS)
+            .withAwaitStrategy(AwaitBuilder.logAwait("Ready to accept connections"))
+            .build()
+
+    data class MyClass(val v: Int)
+
+    @Test
+    fun test() = runBlocking {
+        val cache = RedisCache<MyClass>("mycache", RedisURI.create("redis://${container.ipAddress}"))
+        val result1 = cache.get("test") { MyClass(10) }
+        val result2 = cache.get("test") { MyClass(10) }
+        assertEquals(10, result1.v)
+        assertEquals(10, result2.v)
+        assertNotSame(result1, result2, "If deserialized, should be a different instance")
+    }
+}
