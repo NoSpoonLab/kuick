4f9c1c25a148921e3a228702e959b620da1af5aa
soywiz <soywiz@gmail.com>
2019-04-05 13:02:43 +0200

    Do not use runBlocking on a squash transaction

--- kuick-repositories-squash/src/jvmMain/kotlin/kuick/repositories/squash/transactions-squash.kt
+++ kuick-repositories-squash/src/jvmMain/kotlin/kuick/repositories/squash/transactions-squash.kt
@@ -2,6 +2,7 @@ package kuick.repositories.squash
 
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import kuick.core.*
 import kuick.db.DomainTransaction
 import kuick.db.DomainTransactionContext
 import kuick.db.DomainTransactionService
@@ -18,31 +19,20 @@ import kotlin.coroutines.coroutineContext
 class DomainTransactionServiceSquash @Inject constructor(val db: DatabaseConnection): DomainTransactionService {
 
     @Suppress("OverridingDeprecatedMember")
+    @UseExperimental(KuickInternalWarning::class)
     override suspend fun <T : Any> transactionNullable(transactionalActions: suspend (DomainTransaction) -> T?): T? {
 
-        if (coroutineContext[DomainTransactionContext.Key] != null) {
+        return if (coroutineContext[DomainTransactionContext.Key] != null) {
             println("Reentrando en transaction {}")
-            return domainTransaction { tr -> transactionalActions(tr) }
+            domainTransaction { tr -> transactionalActions(tr) }
         } else {
-            val preTrCtx = coroutineContext
-            return db.transaction {
-                val domainTransaction = DomainTransactionSquash(this)
-                try {
-                    val result = runBlocking(preTrCtx) {
-                        withContext(DomainTransactionContext(domainTransaction)) {
-                            transactionalActions(domainTransaction)
-                        }
-                    }
-                    // TODO Pendiente resolver autoCommit=false
-                    //this.commit()
-                    result
-                } catch (t: Throwable) {
-                    //this.rollback()
-                    throw t
+            db.createTransaction().use { transaction ->
+                val domainTransaction = DomainTransactionSquash(transaction)
+                withContext(DomainTransactionContext(domainTransaction)) {
+                    transactionalActions(domainTransaction)
                 }
             }
         }
-
     }
 
 }
