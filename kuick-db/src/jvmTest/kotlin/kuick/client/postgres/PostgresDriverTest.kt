c7842685a45aa3db9d8939971d04a149365454ad
soywiz <soywiz@gmail.com>
2019-04-08 23:05:57 +0200

    More work

--- kuick-db/src/jvmTest/kotlin/kuick/client/postgres/PostgresDriverTest.kt
+++ kuick-db/src/jvmTest/kotlin/kuick/client/postgres/PostgresDriverTest.kt
@@ -2,14 +2,28 @@ package kuick.client.postgres
 
 import kotlinx.coroutines.*
 import kuick.client.db.*
+import org.arquillian.cube.containerobject.*
+import org.arquillian.cube.docker.impl.client.containerobject.dsl.*
+import org.jboss.arquillian.junit.*
+import org.junit.runner.*
 import kotlin.test.*
 
+@RunWith(Arquillian::class)
 class PostgresDriverTest {
+
+    @DockerContainer
+    var container = Container.withContainerName("kuick-postgres-test")
+            .fromImage("postgres:11.2")
+            .withPortBinding(5432)
+            .withConnectionMode(ConnectionMode.START_AND_STOP_AROUND_CLASS)
+            .withAwaitStrategy(AwaitBuilder.logAwait("database system is ready to accept connections"))
+            .build()
+
     @Test
-    @Ignore
+    //@Ignore
     fun test() {
         runBlocking {
-            PostgresDriver.connect("postgres://postgres@localhost:5432/postgres").use { connection ->
+            PostgresDriver.connect("postgres://postgres@${container.ipAddress}:5432/postgres").use { connection ->
                 val sql = connection.sql
                 connection.transaction { transaction ->
                     kotlin.runCatching {
