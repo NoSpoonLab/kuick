4f70517254f10e30fe0736f950e8bb56e784b25c
mike <maballesteros@gmail.com>
2020-08-03 10:58:28 +0200

    - Remove unused subprojects - Upgraded Gradle wrapper - Upgraded to kotlin 1.3.72

--- samples/todo-app2/src/jvmMain/kotlin/kuick/samples/todo2/persistence.kt
+++
@@ -1,8 +0,0 @@
-package kuick.samples.todo2
-
-import kuick.repositories.ModelRepository
-
-
-interface TodoRepository : ModelRepository<Todo.Id, Todo>
-
-interface UserRepository : ModelRepository<User.Id, User>
